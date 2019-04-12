559341b342e53419f0f09809b1c5f78c25c920ee
Dejan Ristic <dejanr77@yahoo.co>
2019-04-12 11:53:04 +0200

    starting point

---
+++ src/packman/Packman.java
@@ -0,0 +1,16 @@
+package packman;
+
+/**
+ *
+ * @author dejan
+ */
+public class Packman {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+
+}
